f717a413b2bdc113f6fd31e6155183b7eec887ba
Neon <32961194+y9neon@users.noreply.github.com>
2021-05-20 09:27:29 +0300

    update: Library group, version moved to Library.kt.

---
+++ buildSrc/src/main/kotlin/Library.kt
@@ -0,0 +1,7 @@
+object Library {
+    const val GROUP = "com.kotlingang.kds"
+    const val VERSION = "1.2.11"
+    const val ARTIFACT_ID = "kds"
+    const val NAME = "Kotlin Data Storage"
+    const val DESCRIPTION = "Multiplatform Coroutine-Based Kotlin Library for storing data via kotlinx.serialization"
+}
