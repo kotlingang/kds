e8df591e35f71423bec0d6de2a4e4d000f000942
Alex Sokol <sokolsokolinskiy@gmail.com>
2021-06-08 00:02:40 +0300

    close #23

---
+++ core/src/commonMain/kotlin/fun/kotlingang/kds/annotation/ExperimentalKDSApi.kt
@@ -0,0 +1,8 @@
+package `fun`.kotlingang.kds.annotation
+
+
+@RequiresOptIn (
+    message = "This API is unstable yet, probably you can see why in the kdoc",
+    level = RequiresOptIn.Level.WARNING
+)
+public annotation class ExperimentalKDSApi
