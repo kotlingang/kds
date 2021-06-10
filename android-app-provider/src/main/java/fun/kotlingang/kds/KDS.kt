7698161bb9f60ab76a6e9fe67f1d4548dbb5097c
Alex Sokol <sokolsokolinskiy@gmail.com>
2021-06-10 22:40:31 +0300

    fixed context bug

--- android-app-provider/src/main/java/fun/kotlingang/kds/KDS.kt
+++ android-app-provider/src/main/java/fun/kotlingang/kds/KDS.kt
@@ -7,10 +7,9 @@ import android.app.Application
 public object KDS {
     private var contextSource: Application? = null
     @InternalKDSApi
-    public val context: Application = contextSource
+    public val context: Application get() = contextSource
         ?: error("Please ensure you called KDS.start() in App.onStart()")
 
-    @OptIn(InternalKDSApi::class)
     public fun onCreate(app: Application) {
         contextSource = app
     }
