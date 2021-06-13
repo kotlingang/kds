7ed2996bd18d21d12cfb9f26704af28635d0dc5f
Alex Sokol <sokolsokolinskiy@gmail.com>
2021-06-13 20:53:43 +0300

    close #28

--- json/json-files/src/commonMain/kotlin/fun/kotlingang/kds/KFileDataStorage.kt
+++ json/json-files/src/commonMain/kotlin/fun/kotlingang/kds/KFileDataStorage.kt
@@ -11,29 +11,16 @@ import kotlinx.serialization.json.Json
 
 
 @OptIn(DelicateCoroutinesApi::class, InternalKDSApi::class)
-public open class KFileDataStorage private constructor (
-    json: Json,
-    private val storage: JsonElementFileDataStorage
-) : KJsonDataStorage(json, storage), AsyncCommittableStorage {
-
-    internal constructor (
-        file: CommonFile,
-        json: Json,
-        scope: CoroutineScope
-    ) : this(json, JsonElementFileDataStorage(json, file, scope))
-
+public expect open class KFileDataStorage : KJsonDataStorage, AsyncCommittableStorage {
     public constructor (
         absolutePath: String,
         json: Json = Json,
         scope: CoroutineScope = GlobalScope + SupervisorJob()
-    ) : this(CommonFile(absolutePath), json, scope)
+    )
 
     public constructor (
         json: Json = Json,
         scope: CoroutineScope = GlobalScope + SupervisorJob()
-    ) : this (
-        CommonFile.HOME_DIR.join(path = "data").join(path = "data.json"),
-        json, scope
     )
 
     public companion object {
@@ -41,34 +28,6 @@ public open class KFileDataStorage private constructor (
             name: String,
             json: Json = Json,
             scope: CoroutineScope = GlobalScope + SupervisorJob()
-        ): KFileDataStorage = KFileDataStorage (
-            CommonFile.HOME_DIR.join(path = "data").join(path = "$name.json"),
-            json, scope
-        )
-    }
-
-    final override suspend fun setup(): Unit = storage.setup()
-    final override fun setupBlocking(): Unit = storage.setupBlocking()
-
-    @OptIn(RawSetterGetter::class, DelicateKDSApi::class)
-    private fun applyMutations() = platformSynchronized(lock = this)  {
-        encodeReferences().forEach { (k, v) ->
-            storage.putJsonElement(k, v)
-        }
-    }
-
-    final override fun launchCommit() {
-        applyMutations()
-        storage.launchCommit()
-    }
-
-    final override suspend fun commit() {
-        applyMutations()
-        storage.commit()
-    }
-
-    final override fun commitBlocking() {
-        applyMutations()
-        storage.commitBlocking()
+        ): KFileDataStorage
     }
 }
--- json/json-files/src/jsMain/kotlin/fun/kotlingang/kds/KFileDataStorage.kt
+++ json/json-files/src/jsMain/kotlin/fun/kotlingang/kds/KFileDataStorage.kt
@@ -2,12 +2,10 @@ package `fun`.kotlingang.kds
 
 import `fun`.kotlingang.kds.file.CommonFile
 import `fun`.kotlingang.kds.storage.AsyncCommittableStorage
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.serialization.json.Json
-import java.io.File
 
 
-@OptIn(DelicateCoroutinesApi::class)
 public actual open class KFileDataStorage private constructor (
     json: Json,
     private val storage: JsonElementFileDataStorage
@@ -22,12 +20,6 @@ public actual open class KFileDataStorage private constructor (
         absolutePath = CommonFile.HOME_DIR.join(path = "data").join(path = "data.json").absolutePath,
         json, scope
     )
-
-    public constructor (
-        file: File,
-        json: Json = Json,
-        scope: CoroutineScope = GlobalScope + SupervisorJob()
-    ) : this(file.absolutePath, json, scope)
 
     public actual companion object {
         public actual fun ofName (
